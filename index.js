22fa8b82fb321900b3c88187a0d152c537b06274
Tom Becker <tjbecker@me.com>
2019-07-03 16:24:07 +0700

    delete spaces

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-
 require('dotenv').config()
 require('./db')
 const app = express()
-
-
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
