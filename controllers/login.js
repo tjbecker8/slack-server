54cdafc2bd505f1eb0215b0555382e39dce44c9f
Tom Becker <tjbecker@me.com>
2019-07-04 12:28:42 +0700

    generate and send token on login

--- controllers/login.js
+++ controllers/login.js
@@ -1,17 +1,22 @@
 const db_user = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
 
 	//check if email exists in database
 	db_user.findOne({email: req.body.email}).then((user)=>{
 		if (user) {
+			//create a token out of interval
 			//if email found, encrypt password,
 			//match passwords encrypted to the one in the database
 			bcrypt.compare(req.body.password, user.password, (err, match) => {
 				if (match) {
-					
-					res.send('you are logged in')
+					let token =jwt.sign(user.toObject(), 'abc') //thats your token
+					res.json({
+						message: 'you are logged in',
+						token: token
+					})
 				} else {
 					res.send('invalid password')
 				}
