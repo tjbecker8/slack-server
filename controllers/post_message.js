ad7cbcee6d67c67bbaf16fe5a694e1d92c6f076f
Tom Becker <tjbecker@me.com>
2019-07-04 13:02:23 +0700

    it works

--- controllers/post_message.js
+++ controllers/post_message.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res) => {
 	jwt.verify(req.body.token, process.env.SECRET, (err, decoded) => {
 		if (decoded) {
-			// console.log('decoded', decoded);
+			console.log('decoded', decoded);
 			req.body.author =decoded._id
 			db_message.create(req.body).then((data) => {
 				res.send(data)
