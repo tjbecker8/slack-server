939822a5dd1919c1de640d626ead4c4bc9fc3c42
Tom Becker <tjbecker@me.com>
2019-07-04 10:45:55 +0700

    encrypting password

--- controllers/post_user.js
+++ controllers/post_user.js
@@ -1,11 +1,20 @@
 const db_user = require('../models/user')
+const bcrypt = require('bcrypt')
 
 module.exports = (req, res) => {
 
-	db_user.create(req.body).then((data) => {
-		res.send(data)
-	}).catch((err)=>{
-		res.send(err)
+	// console.log('req.body before', req.body);
+
+	bcrypt.hash(req.body.password, 10, (err, encrypted) => {
+		if (err) {
+			res.status(300).send('error', err)
+		} else {
+			req.body.password = encrypted
+			db_user.create(req.body).then((data) => {
+				res.send(data)
+			}).catch((err)=>{
+				res.send(err)
+			})
+		}
 	})
-	// res.send('message created')
 }
