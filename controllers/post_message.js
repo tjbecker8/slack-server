dfb2fc8da67c4034164345bddac802a0c7433971
Tom Becker <tjbecker@me.com>
2019-07-08 09:57:51 +0700

    put token in header

--- controllers/post_message.js
+++ controllers/post_message.js
@@ -2,13 +2,14 @@ const db_message = require('../models/message')
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
-	jwt.verify(req.body.token, process.env.SECRET, (err, decoded) => {
+	let token = req.headers.authorization.split(' ')[1]
+	jwt.verify(token, process.env.SECRET, (err, decoded) => {
 		if (decoded) {
 			console.log('decoded', decoded);
 			req.body.author = decoded._id
 			db_message.create(req.body).then((data) => {
 				// res.send(data)
-				console.log('data', data);
+				// console.log('data', data);
 				db_message.findById(data._id).populate('author').then((found_message) => {
 					res.send(found_message)
 				})
