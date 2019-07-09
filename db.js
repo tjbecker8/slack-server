5e18c697cb6da62910d70eca7ba572abcf163430
Tom Becker <tjbecker@me.com>
2019-07-09 09:07:04 +0700

    k

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/slack', {useNewUrlParser: true}, (err) => {
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}, (err) => {
 	if (err) {
 		console.log('err', err);
 	} else {
